52a98954ca3d25a130df24cbaa5937a20de8b480
Thiago Franck <thiagohenrique776@gmail.com>
2021-06-07 15:28:33 -0300

    useMemo and useCallback studies

---
+++ components/ProductItem.tsx
@@ -0,0 +1,24 @@
+import { memo } from 'react';
+
+interface ProductItemProps {
+  product: {
+    id: number;
+    price: number;
+    priceFormatted: string; 
+    title: string;
+  }
+  onAddToWishList: (id: number) => void;
+}
+
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  return (
+    <div>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => onAddToWishList(product.id)}>Add to wishlist</button>
+    </div>
+  )
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
---
+++ components/SearchResults.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react'
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+  totalPrice: number;
+  results: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>
+
+  onAddToWishList: (id: number) => void;
+}
+
+export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
+  
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+
+      {results.map(product => {
+        return (
+          <ProductItem 
+            key={product.id}
+            product={product}
+            onAddToWishList={onAddToWishList}
+          />
+        );
+      })}
+    </div>
+  );
+}
